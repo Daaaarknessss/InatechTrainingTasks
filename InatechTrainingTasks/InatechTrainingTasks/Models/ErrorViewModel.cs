4d9cca7c43697ea61f903f186319a06ab95436a8
Naman Mehta <90234053+Daaaarknessss@users.noreply.github.com>
2023-02-25 15:51:42 +0530

    Delete InatechTrainingTasks/InatechTrainingTasks/Models directory

--- InatechTrainingTasks/InatechTrainingTasks/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KoreMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
