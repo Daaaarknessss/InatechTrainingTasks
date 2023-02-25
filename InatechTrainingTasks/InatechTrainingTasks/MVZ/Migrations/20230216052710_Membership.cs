ed6e3dd57f7c3150388c4a5f270875c3ff5b6365
Naman Mehta <90234053+Daaaarknessss@users.noreply.github.com>
2023-02-25 15:55:41 +0530

    Add files via upload

---
+++ InatechTrainingTasks/InatechTrainingTasks/MVZ/Migrations/20230216052710_Membership.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KoreMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Membership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MembershipFee",
+                columns: table => new
+                {
+                    MembershiptypeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MembershipCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fee = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipFee", x => x.MembershiptypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membership",
+                columns: table => new
+                {
+                    MID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<int>(type: "int", nullable: false),
+                    MembershipTypeID = table.Column<int>(type: "int", nullable: false),
+                    MembershipTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membership", x => x.MID);
+                    table.ForeignKey(
+                        name: "FK_Membership_Customer_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Membership_MembershipFee_MembershipTypeID",
+                        column: x => x.MembershipTypeID,
+                        principalTable: "MembershipFee",
+                        principalColumn: "MembershiptypeID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membership_CustomerID",
+                table: "Membership",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membership_MembershipTypeID",
+                table: "Membership",
+                column: "MembershipTypeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Membership");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "MembershipFee");
+        }
+    }
+}
